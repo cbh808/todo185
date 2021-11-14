7af31496218a0cded918c9902314e289cff28e38
cbh808 <cbh808@gmail.com>
2021-11-15 00:36:05 +0100

    counting logic moved to db

--- database_persistence.rb
+++ database_persistence.rb
@@ -2,7 +2,6 @@ require "pg"
 
 class DatabasePersistence
   def initialize(logger)
-    # @db=>connection to db
     @db = if Sinatra::Base.production?
       PG.connect(ENV['DATABASE_URL'])
     else
@@ -14,40 +13,45 @@ class DatabasePersistence
   def disconnect
     @db.close
   end
-  # method as wrapper for calling exec_params
-  # empty '*parmas' ok in case no $1.. params in sql statement
-  # *params is an array, so don't have to pass array into it
+
   def query(statement, *params)
     @logger.info("#{statement}: #{params}")
     @db.exec_params(statement, params)
   end
 
   def find_list(id)
-    sql = "SELECT * FROM lists WHERE id = $1"
+    sql = <<~SQL
+      SELECT lists.*,
+        COUNT(todos.id) AS todos_count,
+        COUNT(NULLIF(todos.completed, true)) AS todos_remaining_count
+        FROM lists
+        LEFT JOIN todos ON todos.list_id = lists.id
+        WHERE lists.id = $1
+        GROUP BY lists.id
+        ORDER BY lists.name;
+    SQL
     result = query(sql, id)
-    
-    tuple = result.first # first element of array
 
-    list_id = tuple["id"].to_i
-    todos = find_todos_for_list(list_id)
-    {id: list_id, name: tuple["name"], todos: todos}
+    tuple_to_list_hash(result.first)
   end
 
-
   def all_lists
-    sql = "SELECT * FROM lists"
+    sql = <<~SQL
+      SELECT lists.*,
+        COUNT(todos.id) AS todos_count,
+        COUNT(NULLIF(todos.completed, true)) AS todos_remaining_count
+        FROM lists
+        LEFT JOIN todos ON todos.list_id = lists.id
+        GROUP BY lists.id
+        ORDER BY lists.name;
+    SQL
     result = query(sql)
-    # convert PG object into a list object
-    # 'map' available because PG has access to Enumerable class
+
     result.map do |tuple|
-      list_id = tuple["id"].to_i
-      todos = find_todos_for_list(list_id)
-      {id: list_id, name: tuple["name"], todos: todos}
+      tuple_to_list_hash(tuple)
     end
   end
 
-  
-  
   def create_new_list(list_name)
     sql = "INSERT INTO lists (name) VALUES ($1)"
     query(sql, list_name)
@@ -82,16 +86,23 @@ class DatabasePersistence
     query(sql, list_id)
   end
 
-  private
-
   def find_todos_for_list(list_id)
     todo_sql = "SELECT * FROM todos WHERE list_id = $1"
     todos_result = query(todo_sql, list_id)
-      
+
     todos_result.map do |todo_tuple|
       { id: todo_tuple["id"].to_i,
         name: todo_tuple["name"],
         completed: todo_tuple["completed"] == "t" }
     end
+  end
+
+  private
+
+  def tuple_to_list_hash(tuple)
+    { id: tuple["id"].to_i,
+      name: tuple["name"],
+      todos_count: tuple["todos_count"].to_i,
+      todos_remaining_count: tuple["todos_remaining_count"].to_i }
   end
 end
--- todo185.rb
+++ todo185.rb
@@ -19,21 +19,13 @@
   
   helpers do
     def list_complete?(list)
-      todos_count(list) > 0 && todos_remaining_count(list) == 0
+      list[:todos_count] > 0 && list[:todos_remaining_count] == 0
     end
   
     def list_class(list)
       "complete" if list_complete?(list)
     end
-  
-    def todos_count(list)
-      list[:todos].size
-    end
-  
-    def todos_remaining_count(list)
-      list[:todos].count { |todo| !todo[:completed] }
-    end
-  
+
     def sort_lists(lists, &block)
       complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list) }
   
@@ -48,7 +40,6 @@
       complete_todos.each(&block)
     end
   end
-  
 
   
   def load_list(id)
@@ -117,6 +108,7 @@
   get "/lists/:id" do
     @list_id = params[:id].to_i
     @list = load_list(@list_id)
+    @todos = @storage.find_todos_for_list(@list_id)
     erb :list, layout: :layout
   end
   
